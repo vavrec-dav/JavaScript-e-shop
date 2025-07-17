6e5d82f9b3af4bc473780e105688a969048b6d7e
David <david@vavrecka.dev>
2025-07-17 15:38:16 +0200

    User maping fix

--- services/user-service/src/repository/userRepository.ts
+++ services/user-service/src/repository/userRepository.ts
@@ -21,8 +21,6 @@ export const updateUser = async (user: UpdateUserRequest): Promise<User> => {
     const query = 'UPDATE users SET first_name = $1, last_name = $2 WHERE id = $3 RETURNING *';
     const values = [user.firstName, user.lastName, user.id];
 
-    console.log(values,user.firstName)
-
     const result = await databasePool.query(query, values).catch(error => {
         console.error('Error updating user:', error);
         throw new Error('Failed to update user');
@@ -33,7 +31,10 @@ export const updateUser = async (user: UpdateUserRequest): Promise<User> => {
 export const getUsers = async (limit: number, offset: number): Promise<User[]> => {
     const query = 'SELECT * FROM users ORDER BY id ASC LIMIT $1 OFFSET $2';
     const values = [limit, offset];
-    return databasePool.query(query, values).then(result => result.rows);
+
+    return databasePool.query(query, values).then(result => result
+        .rows
+        .map(row => mapRowToUser(row)));
 }
 
 export const getTotalCountOfUsers = async (): Promise<number> => {
@@ -48,7 +49,7 @@ export const getUserById = async (id: string): Promise<User | null> => {
 
     const result = await databasePool.query(query, values);
     if (result.rows.length > 0) {
-        return result.rows[0];
+        return mapRowToUser(result.rows[0]);
     }
     return null;
 }
@@ -62,4 +63,13 @@ export const createUser = async (user: User): Promise<User> => {
         throw new Error('Failed to create user');
     });
     return result.rows[0];
+}
+
+function mapRowToUser(row: any): User {
+    return {
+        id: row.id,
+        firstName: row.first_name,
+        lastName: row.last_name,
+        createdAt: row.created_at
+    };
 }
